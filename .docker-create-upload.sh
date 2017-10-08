97d6d92d173dbeb4361f8c339780bc30fa98140b
Florent Clairambault <florent@clairambault.fr>
2017-10-08 02:08:23 +0200

    Container creation: Let's see how it goes

--- .docker-create-upload.sh
+++ .docker-create-upload.sh
@@ -1,3 +1,25 @@
-#!/bin/sh
+#!/bin/sh -e
 
-docker build -t fclairamb/ftpserver .
+docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+
+set -x
+
+DOCKER_NAME=${DOCKER_REPO}:${TRAVIS_COMMIT::7}
+
+docker build -t ${DOCKER_NAME} .
+
+docker tag ${DOCKER_NAME} ${DOCKER_REPO}:travis-${TRAVIS_BUILD_NUMBER}
+
+if [ "${TRAVIS_TAG}" = "" ]; then
+    if [ "${TRAVIS_BRANCH}" = "master" ]; then
+        DOCKER_TAG=latest
+    else
+        DOCKER_TAG=${TRAVIS_BRANCH}
+    fi
+else
+    DOCKER_TAG=${TRAVIS_TAG}
+fi
+
+docker tag ${DOCKER_NAME} ${DOCKER_REPO}:${DOCKER_TAG}
+
+docker push ${DOCKER_REPO}
