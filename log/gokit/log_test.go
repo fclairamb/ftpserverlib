afe2a62b465e97525b79369e346cf468df9c7106
Florent Clairambault <florent@habx.fr>
2020-07-13 00:02:37 +0200

    Cleaning up the gokit logger API

--- log/gokit/log_test.go
+++ log/gokit/log_test.go
@@ -1,18 +1,15 @@
 package gokit
 
 import (
-	"os"
 	"testing"
-
-	gklog "github.com/go-kit/kit/log"
 
 	"github.com/fclairamb/ftpserverlib/log"
 )
 
 func getLogger() log.Logger {
-	return NewGKLogger(gklog.NewLogfmtLogger(gklog.NewSyncWriter(os.Stdout))).With(
-		"ts", gklog.DefaultTimestampUTC,
-		"caller", gklog.DefaultCaller,
+	return NewGKLoggerStdout().With(
+		"ts", GKDefaultTimestampUTC,
+		"caller", GKDefaultCaller,
 	)
 }
 
