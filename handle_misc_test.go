5a5926a7b45541009fb082aa4e2acaaad08fdd34
Nicola Murino <nicola.murino@gmail.com>
2022-12-04 08:21:58 +0100

    add some tests

--- handle_misc_test.go
+++ handle_misc_test.go
@@ -297,6 +297,22 @@ func TestTYPE(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, StatusOK, rc)
 
+	rc, _, err = raw.SendCommand("TYPE i")
+	require.NoError(t, err)
+	require.Equal(t, StatusOK, rc)
+
+	rc, _, err = raw.SendCommand("TYPE a")
+	require.NoError(t, err)
+	require.Equal(t, StatusOK, rc)
+
+	rc, _, err = raw.SendCommand("TYPE l 8")
+	require.NoError(t, err)
+	require.Equal(t, StatusOK, rc)
+
+	rc, _, err = raw.SendCommand("TYPE l 7")
+	require.NoError(t, err)
+	require.Equal(t, StatusOK, rc)
+
 	rc, _, err = raw.SendCommand("TYPE wrong")
 	require.NoError(t, err)
 	require.Equal(t, StatusNotImplementedParam, rc)
